d80388b6c452d161360216b1c0a58a0df190456b
vagrant <vagrant@vagrant.vm>
2018-05-11 11:43:46 +0000

    Added mobile view and sample data file

--- create_database.py
+++ create_database.py
@@ -26,7 +26,7 @@ class Categories(Base):
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    
+
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
